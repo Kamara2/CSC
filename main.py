20330be28d02ba8c6ebba25a7847835666c7e85c
Kamara2 <93331023+Kamara2@users.noreply.github.com>
2022-05-25 21:25:11 +0100

    Add files via upload
    Kamara is still learning to commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def bio_data():
+ name = str(input("Inert your name below"))
+ age = int(input("insert your age below"))
+ matric_number = int(input("insert matric number"))
+
+ print("You are")
+ print(name)
+ print(age)
+ print(matric_number)
+
+bio_data()
